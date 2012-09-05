5800af12debda505978d0380ed35da68646d6af4
Alan James <alan@Tower.(none)>
2012-09-05 17:02:41 -0400

    Working on the CSS. Making it more OOP.

--- framework/core/autoload.php
+++ framework/core/autoload.php
@@ -1,10 +1,10 @@
 <?php if(!defined('SERVER_ROOT')){header('/error_404');exit;}
 
-foreach($AUTOLOAD_LIB_FRAMEWORK as $php_framework){
+foreach($AUTOLOAD_PHP_FRAMEWORK as $php_framework){
   MLoad::php_framework($php_framework);
 }
 
-foreach($AUTOLOAD_LIB_APPLICATION as $php_application){
+foreach($AUTOLOAD_PHP_APPLICATION as $php_application){
   MLoad::php_application($php_application);
 }
 
--- framework/lib/php/util/MTime.php
+++ framework/lib/php/util/MTime.php
@@ -7,44 +7,66 @@
 */
 
 class MTime {
-
-  public static function sec2hms ($sec) 
-  {
-
+  
+  public static function sec2hms($sec){
+    
     // start with a blank string
     $hms = '';
-    
+
     // do the hours first: there are 3600 seconds in an hour, so if we divide
     // the total number of seconds by 3600 and throw away the remainder, we're
     // left with the number of hours in those seconds
-    $hours = intval(intval($sec) / 3600); 
-    
+    $hours = intval($sec / 3600); 
+
     // dividing the total seconds by 60 will give us the number of minutes
     // in total, but we're interested in *minutes past the hour* and to get
     // this, we have to divide by 60 again and then use the remainder
-    $minutes = intval(($sec / 60) % 60); 
+    $minutes = intval($sec / 60) % 60; 
 
     // seconds past the minute are found by dividing the total number of seconds
     // by 60 and using the remainder
-    $seconds = intval($sec % 60); 
+    $seconds = $sec % 60; 
 
-    // add hours to $hms
-    if($hours > 0){
-      $hms .= str_pad($hours, 2, '0', STR_PAD_LEFT). ':';
+    //Padding
+    //Hours
+    $hours_pad = '';
+
+    if($hours < 10){
+      $hours_pad = '0';
     }
-    
-    // add minutes to $hms
-    if($hours > 0 || $minutes > 0){
-      $hms .= str_pad($minutes, 2, '0', STR_PAD_LEFT). ':';
+
+    //Minutes
+    $minutes_pad = '';
+
+    if($minutes < 10){
+    $minutes_pad = '0';
     }
-    
-    // add seconds to $hms
-    if($minutes > 0 || $seconds > 0){
-      $hms .= str_pad($seconds, 2, '0', STR_PAD_LEFT);
+
+    //Seconds
+    $seconds_pad = '';
+
+    if($seconds < 10){
+      $seconds_pad = '0';
+    }
+
+    //Output
+    //If there are hours
+    if($hours >= 0) {
+      $hms = $hours_pad + $hours + ':' + $minutes_pad + $minutes + ':' + $seconds_pad + $seconds;
+    }
+
+    //If there are no hours but there are mins
+    if($hours <= 0 && $minutes > 0) {
+      $hms = $minutes_pad + $minutes + ':' + $seconds_pad + $seconds;
+    }
+
+    //If there are no hours and no minutes
+    //This will display 00 if there are 0 seconds
+    if($hours <= 0 && $minutes <= 0) {
+      $hms = $seconds_pad + $seconds;
     }
 
     return $hms;
-    
   }
   
 }
