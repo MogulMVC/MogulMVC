d38cc4930a8794d39229683d025fd59b38a29477
Alan James <alan@Tower.(none)>
2012-09-10 19:54:45 -0400

    Changed FRAMEWORK_SWF and APPLICATION_SWF to FRAMEWORK_LIB_AS and APPLICATION_LIB_AS.

--- framework/core/config.php
+++ framework/core/config.php
@@ -5,10 +5,10 @@ date_default_timezone_set('America/New_York');
 define('FRAMEWORK_VERSION', "120808");
 
 define('FRAMEWORK_IMG', 'img');
+define('FRAMEWORK_LIB_AS', 'lib/as');
 define('FRAMEWORK_LIB_CSS', 'lib/css');
 define('FRAMEWORK_LIB_JS', 'lib/js');
 define('FRAMEWORK_LIB_PHP', 'lib/php');
-define('FRAMEWORK_SWF', 'swf');
 define('FRAMEWORK_TEMPLATE', 'template');
 define('FRAMEWORK_VID', 'vid');
 
--- framework/template/video_player.php
+++ framework/template/video_player.php
@@ -18,19 +18,19 @@ if($autostart){
 ?>
 
 <object width="<?php echo $width;?>" height="<?php echo $height;?>" classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000">
-  <param name="movie" value="<?php echo '/' . FRAMEWORK . '/' . FRAMEWORK_SWF; ?>/video_player.swf" />
+  <param name="movie" value="<?php echo '/' . FRAMEWORK . '/' . FRAMEWORK_LIB_AS; ?>/video_player.swf" />
   <param name="allowfullscreen" value="true" />
   <param name="allowscriptaccess" value="always" />
   <param name="wmode" value="transparent" />
-  <param name="flashvars" value="file=<?php echo $file;?><?php echo $file_hd_string; ?><?php echo $thumbnail_srting ?><?php echo $autostart_string ?>&dock=false&controlbar.position=over&skin=<?php echo '/' . FRAMEWORK . '/' . FRAMEWORK_SWF; ?>/mogul.zip" />
+  <param name="flashvars" value="file=<?php echo $file;?><?php echo $file_hd_string; ?><?php echo $thumbnail_srting ?><?php echo $autostart_string ?>&dock=false&controlbar.position=over&skin=<?php echo '/' . FRAMEWORK . '/' . FRAMEWORK_LIB_AS; ?>/mogul.zip" />
   <embed
     type="application/x-shockwave-flash"
     width="<?php echo $width;?>"
     height="<?php echo $height;?>"
-    src="<?php echo '/' . FRAMEWORK . '/' . FRAMEWORK_SWF; ?>/video_player.swf"
+    src="<?php echo '/' . FRAMEWORK . '/' . FRAMEWORK_LIB_AS; ?>/video_player.swf"
     allowfullscreen="true"
     allowscriptaccess="always"
     wmode="transparent"
-    flashvars="file=<?php echo $file;?><?php echo $file_hd_string; ?><?php echo $thumbnail_srting ?><?php echo $autostart_string ?>&dock=false&controlbar.position=over&skin=<?php echo '/' . FRAMEWORK . '/' . FRAMEWORK_SWF; ?>/mogul.zip"
+    flashvars="file=<?php echo $file;?><?php echo $file_hd_string; ?><?php echo $thumbnail_srting ?><?php echo $autostart_string ?>&dock=false&controlbar.position=over&skin=<?php echo '/' . FRAMEWORK . '/' . FRAMEWORK_LIB_AS; ?>/mogul.zip"
   />
 </object>
