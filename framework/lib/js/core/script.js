ffbc654d24e496e75daad10fc274675733e1aa32
Alan James <alanjames1987@gmail.com>
2012-09-26 18:05:02 -0400

    Auto Commit

--- framework/lib/js/core/script.js
+++ framework/lib/js/core/script.js
@@ -101,38 +101,98 @@ var rounded_large = '2em';$(window).load(function() {
 
 })(window);(function() {
 
-	function MListFadeIn() {
-		var i = 0;
-		$(".MListItem").each(function() {
-			$(this).delay(i).animate({
-				opacity : 1
-			}, speedNorm);
-			i = i + 50;
-		});
+	var MList = {
+		fadeIn : function() {
+			var i = 0;
+			$(".MListItem").each(function() {
+				$(this).delay(i).animate({
+					opacity : 1
+				}, speedNorm);
+				i = i + 50;
+			});
 
-		var i = 0;
-		$("#MSidebar li").each(function() {
-			$(this).delay(i).animate({
-				opacity : 1
-			}, speedNorm);
-			i = i + 50;
-		});
-	};
+			var i = 0;
+			$("#MSidebar li").each(function() {
+				$(this).delay(i).animate({
+					opacity : 1
+				}, speedNorm);
+				i = i + 50;
+			});
+		},
+		selectAll : function(selector) {
+			console.log(selector);
+			$(selector + " .MListItem input[type=checkbox]:visible").each(function() {
+				$(this).attr("checked", "checked");
+				MList.updateUI(selector);
+			});
+		},
+		deselectAll : function(selector) {
+			console.log(selector);
+			$(selector + ' .MListItem input[type=checkbox]').each(function() {
+				$(this).removeAttr('checked');
+				MList.updateUI(selector);
+			});
+		},
+		updateUI : function(selector) {
 
-	//Fix Later
-	function MListSelect(id) {
-		if (document.getElementById("checkbox_" + id).checked) {
-			$("#list_item_container_" + id).addClass("blue");
-		} else {
-			$("#list_item_container_" + id).removeClass("blue");
+			// Check if anything was passed in
+			if (selector === undefined) {
+				selector = '';
+			}
+
+			$(selector + '.MListItem input[type=checkbox]').each(function(index) {
+
+				// Set the default state
+				if ($(this).is(":checked")) {
+					$(this).closest('.MListItem').addClass('blue');
+				}else{
+					$(this).closest('.MListItem').removeClass('blue');
+				}
+
+				// Add a change event listener
+				// A label can change the state of the checkbox so I am using change instead of click
+				$(this).live('change', function() {
+					if ($(this).is(":checked")) {
+						$(this).closest('.MListItem').addClass('blue');
+					} else {
+						$(this).closest('.MListItem').removeClass('blue');
+					}
+				});
+
+			});
+
 		}
 	};
 
-	$(window).load(function() {
-		MListFadeIn();
-	});
+	window.MList = MList;
 
 })();
+
+// Setup checked when the page loads
+$(document).ready(function() {
+
+	$('.MListItem input[type=checkbox]').each(function(index) {
+
+		// Set the default state
+		MList.updateUI();
+
+		// Add a change event listener
+		// A label can change the state of the checkbox so I am using change instead of click
+		$(this).live('change', function() {
+			if ($(this).is(":checked")) {
+				$(this).closest('.MListItem').addClass('blue');
+			} else {
+				$(this).closest('.MListItem').removeClass('blue');
+			}
+		});
+
+	});
+
+});
+
+$(window).load(function() {
+	MList.fadeIn();
+});
 (function() {
 
 	$(window).load(function() {
