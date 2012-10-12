ab60c4e66325cb40d4ac5c7a8ec50ad28eafea69
Alan James <alanjames1987@gmail.com>
2012-10-12 02:12:55 -0400

    Updates.

--- framework/lib/js/core/MScript.js
+++ framework/lib/js/core/MScript.js
@@ -86,7 +86,7 @@ $(document).ready(function() {
 	}, speedSlow);
 });
 $(document).ready(function() {
-	$('.MNote').each(function() {
+	$('.MNote, .MNoteRed, .MNoteR, .MNoteOrange, .MNoteO, .MNoteYellow, .MNoteY, .MNoteGreen, .MNoteG, .MNoteBlue, .MNoteB, .MNoteViolet, .MNoteV').each(function() {
 		var rotation = MMath.random(-8, 8);
 		$(this).css({
 			'-webkit-transform' : 'rotate(' + rotation + 'deg)',
