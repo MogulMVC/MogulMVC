2374d234e7c872b7e43a8bcefa705bbc74aeac26
Alan James <alanjames1987@gmail.com>
2012-10-15 21:12:28 -0400

    Worked on some CLI stuff.

--- framework/lib/php/core/util/MCLI.php
+++ framework/lib/php/core/util/MCLI.php
@@ -3,7 +3,8 @@
 class MCLI {
 
 	public static function cli_is() {
-		if (php_sapi_name() == 'cli' && empty($_SERVER['REMOTE_ADDR'])) {
+		// If from command line or from Cron
+		if (php_sapi_name() == 'cli' || php_sapi_name() == cgi-fcgi) {
 			return TRUE;
 		} else {
 			return FALSE;
