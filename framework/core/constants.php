5826ba0559b2c6d060c1646b6d62b64da63af590
Alan James <alanjames1987@gmail.com>
2012-10-15 22:07:27 -0400

    Auto Commit

--- framework/core/constants.php
+++ framework/core/constants.php
@@ -3,7 +3,7 @@ if (!defined('SERVER_ROOT')) {header('/error_404');
 	exit ;
 }
 
-if (php_sapi_name() == 'cli' || php_sapi_name() == cgi-fcgi) {
+if (php_sapi_name() == 'cli' || php_sapi_name() == 'cgi-fcgi') {
 	define('NL', '\n');
 } else {
 	define('NL', '<br />');
--- framework/lib/php/core/util/MCLI.php
+++ framework/lib/php/core/util/MCLI.php
@@ -3,8 +3,9 @@
 class MCLI {
 
 	public static function cli_is() {
-		// If from command line or from Cron
-		if (php_sapi_name() == 'cli' || php_sapi_name() == cgi-fcgi) {
+		// if (php_sapi_name() == 'cli') {
+		// if the remote addr is empty it is run by the cli or cron
+		if (empty($_SERVER['REMOTE_ADDR'])) {
 			return TRUE;
 		} else {
 			return FALSE;
