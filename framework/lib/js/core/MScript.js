9519ca6188653b5b51c6a857d0b53fa335f54977
Alan James <alanjames1987@gmail.com>
2012-11-24 21:39:46 -0500

    Commit

--- framework/lib/js/core/MScript.js
+++ framework/lib/js/core/MScript.js
@@ -80,10 +80,6 @@ $(document).ready(function() {
 	});
 });$(document).ready(function() {
 
-	$('.MPopupContainer').live('click', function() {
-		$(this).hide();
-	});
-
 	$('.MPopup .MIconClose').live('click', function() {
 		$(this).closest('.MPopupContainer').hide();
 	});
