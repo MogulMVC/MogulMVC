5db7d6893588036bb371811dfa2a9378baf85be1
Alan James <alanjames1987@gmail.com>
2012-11-27 16:50:21 -0500

    Popups are now draggable.

--- application/config/config.php
+++ application/config/config.php
@@ -11,7 +11,7 @@ define('APPLICATION_NAME', 'Application Name');
 define('APPLICATION_DESCRIPTION', 'Application Description');
 define('APPLICATION_ID', 'SomeUniqueID');
 define('APPLICATION_PASSWORD', 'mogulpass321456');
-define('APPLICATION_VERSION', '121125');
+define('APPLICATION_VERSION', '121127');
 
 // Environment
 define('APPLICATION_ENVIRONMENT', 'development');
