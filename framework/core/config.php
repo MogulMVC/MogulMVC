26005816395cc038631ad73d2fbeb5d589afa22b
Alan James <alanjames1987@gmail.com>
2012-11-30 16:28:33 -0500

    Fixed some javascript stuff.

--- framework/core/config.php
+++ framework/core/config.php
@@ -3,7 +3,7 @@ if (!defined('SERVER_ROOT')) {header('/error_404');
 	exit ;
 }
 
-define('FRAMEWORK_VERSION', '121128');
+define('FRAMEWORK_VERSION', '121130');
 
 define('FRAMEWORK_IMG', 'img');
 define('FRAMEWORK_LIB_AS', 'lib/as');
