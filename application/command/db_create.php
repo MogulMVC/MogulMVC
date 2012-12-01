2711c1f8674580d4f3b1a9981e7923f68e4e63f3
Alan James <alanjames1987@gmail.com>
2012-12-01 03:38:41 -0500

    Commit

--- application/command/db_create.php
+++ application/command/db_create.php
@@ -4,12 +4,12 @@ MLoad::php_framework('core/db/MPDO');
 
 $DB = $GLOBALS['MPDO'][0];
 
-$query = $DB -> prepare("CREATE TABLE IF NOT EXISTS tableName (
+$query = $DB -> prepare('CREATE TABLE IF NOT EXISTS tableName (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   
   created DATETIME,
   updated DATETIME
-)");
+)');
 $query -> execute();
 
 echo 'Tables Created';
