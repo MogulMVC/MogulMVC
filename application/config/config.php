bb676b011875d8d4324321cd3164a9bea54e3b48
Alan James <alanjames1987@gmail.com>
2013-01-06 13:05:46 -0500

    Updated the CSS.

--- application/config/config.php
+++ application/config/config.php
@@ -11,7 +11,7 @@ define('APPLICATION_NAME', 'Application Name');
 define('APPLICATION_DESCRIPTION', 'Application Description');
 define('APPLICATION_ID', 'SomeUniqueID');
 define('APPLICATION_PASSWORD', 'mogulpass321456');
-define('APPLICATION_VERSION', '121230');
+define('APPLICATION_VERSION', '130106');
 
 // DOCTYPE
 // 'xhtml11' 'xhtml1-strict' 'xhtml1-transitional' 'xhtml1-frame' 'html5' 'html4-strict' 'html4-transitional' 'html4-frame'
