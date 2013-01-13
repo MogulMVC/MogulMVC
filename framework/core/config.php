73138b93cf15d2418a1ec4e418a853630eef9444
Alan James <alanjames1987@gmail.com>
2013-01-13 18:41:01 -0500

    Commit

--- framework/core/config.php
+++ framework/core/config.php
@@ -3,7 +3,7 @@ if (!defined('SERVER_ROOT')) {header('/error_404');
 	exit ;
 }
 
-define('FRAMEWORK_VERSION', '130106');
+define('FRAMEWORK_VERSION', '130113');
 
 define('FRAMEWORK_IMG', 'img');
 define('FRAMEWORK_LIB_AS', 'lib/as');
