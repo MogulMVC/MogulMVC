fcd4816f15048c2d34f16ac91adeb869972d920a
Alan James <alanjames1987@gmail.com>
2013-02-12 16:05:35 -0500

    Changed the session system. It now saves sessions as JSON objects to support cross language sessions.

--- framework/core/config.php
+++ framework/core/config.php
@@ -10,7 +10,7 @@ if (!defined('SERVER_ROOT')) {header('/error_404');
 	exit ;
 }
 
-define('FRAMEWORK_VERSION', '130211');
+define('FRAMEWORK_VERSION', '130212');
 
 define('FRAMEWORK_IMG', 'img');
 define('FRAMEWORK_LIB_AS', 'lib/as');
--- framework/lib/php/core/db/MSession.php
+++ framework/lib/php/core/db/MSession.php
@@ -23,74 +23,107 @@ if (!empty($_SERVER['REMOTE_ADDR'])) {
 		}
 
 		// Define the callback functions
-		function _open() {
-			
+		function MSessionOpen() {
+
 			if (!isset($_COOKIE['session_id'])) {
 				// Set a cookie to hold the session id
 				setcookie('session_id', session_id(), time() + 60 * 60 * 24 * 365, '/', MURL::domain());
 			}
-			
+
 			if (isset($_COOKIE['session_id'])) {
 				session_id($_COOKIE['session_id']);
 			}
-			
+
 		}
 
-		function _close() {
+		function MSessionClose() {
+
 		}
 
-		function _read($id) {
+		function MSessionRead($id) {
 
 			$DB = new PDO($GLOBALS['SESSION_TYPE'] . ':host=' . $GLOBALS['SESSION_HOST'] . ';dbname=' . $GLOBALS['SESSION_NAME'], $GLOBALS['SESSION_USER'], $GLOBALS['SESSION_PASS']);
 
-			$query = $DB -> prepare("SELECT * from " . $GLOBALS['SESSION_TABLE'] . " WHERE id = ?");
+			$query = $DB -> prepare('SELECT * from ' . $GLOBALS['SESSION_TABLE'] . ' WHERE id = ?');
 			$query -> execute(array($id));
 			$result = $query -> fetch();
 
 			$DB = null;
 
-			return $result['data'];
+			// I don't understand the following code yet
+			// but it works with JSON in the database
+			// so I am happy for now
+			$tmp = $_SESSION;
+
+			$temp = json_decode($result['data']);
+			$_SESSION = (array) $temp;
+
+			if (isset($_SESSION) && !empty($_SESSION) && $_SESSION != null) {
+
+				$new_data = session_encode();
+				$_SESSION = $tmp;
+				return $new_data;
+
+			} else {
+
+				return "";
+
+			}
 
 		}
 
-		function _write($id, $data) {
+		function MSessionWrite($id, $data) {
 
+			// Store the original session
+			$original_session = $_SESSION;
+
+			// Decode the session and store it back into $_SESSION
+			session_decode($data);
+
+			// Turn decoded session into JSON
+			$data = json_encode($_SESSION);
+
+			// Change the session data back to the old session
+			// The JSON will be stored in the database while PHP keeps its original session
+			$_SESSION = $original_session;
+
+			// Create an access time
 			$access = time();
 
 			$DB = new PDO($GLOBALS['SESSION_TYPE'] . ':host=' . $GLOBALS['SESSION_HOST'] . ';dbname=' . $GLOBALS['SESSION_NAME'], $GLOBALS['SESSION_USER'], $GLOBALS['SESSION_PASS']);
 
-			$query = $DB -> prepare("REPLACE INTO " . $GLOBALS['SESSION_TABLE'] . " VALUES  (?, ?, ?)");
+			$query = $DB -> prepare('REPLACE INTO ' . $GLOBALS['SESSION_TABLE'] . ' VALUES  (?, ?, ?)');
 			$query -> execute(array($id, $access, $data));
 
 			$DB = null;
 
 		}
 
-		function _destroy($id) {
+		function MSessionDestroy($id) {
 
 			$DB = new PDO($GLOBALS['SESSION_TYPE'] . ':host=' . $GLOBALS['SESSION_HOST'] . ';dbname=' . $GLOBALS['SESSION_NAME'], $GLOBALS['SESSION_USER'], $GLOBALS['SESSION_PASS']);
 
-			$query = $DB -> prepare("DELETE from " . $GLOBALS['SESSION_TABLE'] . " WHERE id = ?");
+			$query = $DB -> prepare('DELETE from ' . $GLOBALS['SESSION_TABLE'] . ' WHERE id = ?');
 			$query -> execute(array($id));
 
 			$DB = null;
 
 		}
 
-		function _clean($max) {
+		function MSessionClean($max) {
 
 			$old = time() - $max;
 
 			$DB = new PDO($GLOBALS['SESSION_TYPE'] . ':host=' . $GLOBALS['SESSION_HOST'] . ';dbname=' . $GLOBALS['SESSION_NAME'], $GLOBALS['SESSION_USER'], $GLOBALS['SESSION_PASS']);
 
-			$query = $DB -> prepare("DELETE from " . $GLOBALS['SESSION_TABLE'] . " WHERE  access < ?");
+			$query = $DB -> prepare('DELETE from ' . $GLOBALS['SESSION_TABLE'] . ' WHERE  access < ?');
 			$query -> execute(array($old));
 
 			$DB = null;
 
 		}
 
-		session_set_save_handler('_open', '_close', '_read', '_write', '_destroy', '_clean');
+		session_set_save_handler('MSessionOpen', 'MSessionClose', 'MSessionRead', 'MSessionWrite', 'MSessionDestroy', 'MSessionClean');
 
 	}
 
