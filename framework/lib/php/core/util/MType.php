05be3cfbbcde74b293b840eba7935f3ea5d07609
Alan James <alanjames1987@gmail.com>
2013-03-24 08:11:23 -0400

    Commit

--- framework/lib/php/core/util/MType.php
+++ framework/lib/php/core/util/MType.php
@@ -22,25 +22,4 @@ class MType {
 
 	}
 
-	function array2Object($array) {
-		
-		if (!is_array($array)) {
-			return $array;
-		}
-
-		$object = new stdClass();
-		if (is_array($array) && count($array) > 0) {
-			foreach ($array as $name => $value) {
-				$name = strtolower(trim($name));
-				if (!empty($name)) {
-					$object -> $name = arrayToObject($value);
-				}
-			}
-			return $object;
-		} else {
-			return FALSE;
-		}
-		
-	}
-
 }
