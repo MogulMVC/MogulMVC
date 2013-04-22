15cfe718f9c6f97f4c5dc8f8ef04175926387b83
Alan James <alanjames1987@gmail.com>
2013-04-21 22:58:20 -0400

    Commit

--- framework/lib/js/core/MScript.js
+++ framework/lib/js/core/MScript.js
@@ -1,6 +1,6 @@
 /*
  * MScript.js by Alan James
- * version 130418
+ * version 130421
  * recommended jQuery version 1.9.0
  */
 
@@ -8,7 +8,7 @@
 
 	var MConfig = {};
 
-	MConfig.version = "130418";
+	MConfig.version = "130421";
 
 	// Speed
 	MConfig.speedFast = 125;
