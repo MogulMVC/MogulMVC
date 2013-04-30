679e7b62870803026d5425f12831a948a7e3f37c
Alan James <alanjames1987@gmail.com>
2013-04-30 15:04:43 -0400

    Commit

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -7,10 +7,10 @@ if (!defined('SERVER_ROOT')) {
 $GLOBALS['AUTOLOAD_PHP_FRAMEWORK'] = array();
 $GLOBALS['AUTOLOAD_PHP_APPLICATION'] = array();
 
-$GLOBALS['AUTOLOAD_CSS_FRAMEWORK'] = array('3rdparty/normalize', 'core/MIcons', 'core/MStyle', 'core/style');
+$GLOBALS['AUTOLOAD_CSS_FRAMEWORK'] = array('vendor/normalize', 'core/MIcons', 'core/MStyle', 'core/style');
 $GLOBALS['AUTOLOAD_CSS_APPLICATION'] = array();
 $GLOBALS['AUTOLOAD_CSS_EXTERNAL'] = array();
 
-$GLOBALS['AUTOLOAD_JS_FRAMEWORK'] = array('3rdparty/jquery', 'core/MScript', 'core/script');
+$GLOBALS['AUTOLOAD_JS_FRAMEWORK'] = array('vendor/jquery', 'core/MScript', 'core/script');
 $GLOBALS['AUTOLOAD_JS_APPLICATION'] = array();
 $GLOBALS['AUTOLOAD_JS_EXTERNAL'] = array();
