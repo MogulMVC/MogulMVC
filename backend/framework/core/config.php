5ef68156a9f50a0b98f12bed3edfd408972f302c
Alan James <alanjames1987@gmail.com>
2013-05-04 14:32:36 -0400

    Commit

--- backend/framework/core/config.php
+++ backend/framework/core/config.php
@@ -2,7 +2,7 @@
 
 /*
  * Mogul MVC by Alan James
- * version 130502
+ * version 130504
  * recommended PHP version 5.3
  */
 
@@ -11,7 +11,7 @@ if (!defined('BACKEND_ROOT')) {
 	exit ;
 }
 
-define('FRAMEWORK_VERSION', '130502');
+define('FRAMEWORK_VERSION', '130504');
 
 // Backend Framework Directories
 define('FRAMEWORK_PHP', 'lib');
