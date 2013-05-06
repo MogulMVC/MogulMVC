fab87fa1fb471963ca3a94e8b7b28b586cd0fb3c
Alan James <alanjames1987@gmail.com>
2013-05-06 04:15:42 -0400

    Commit

--- backend/application/view/template/head.php
+++ backend/application/view/template/head.php
@@ -10,19 +10,19 @@ echo $GLOBALS['DOCTYPE']['html4-transitional'];
 	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
 	
 	<?php if(!empty($page_title)): ?>
-		<title><?php echo $page_title . ' - ' . APPLICATION_NAME; ?></title>
-		<meta name="title" content="<?php echo $page_title . ' - ' . APPLICATION_NAME; ?>" />
+	<title><?php echo $page_title . ' - ' . APPLICATION_NAME; ?></title>
+	<meta name="title" content="<?php echo $page_title . ' - ' . APPLICATION_NAME; ?>" />
 	<?php else: ?>
-		<title><?php echo APPLICATION_NAME; ?></title>
-		<meta name="title" content="<?php echo APPLICATION_NAME; ?>" />
+	<title><?php echo APPLICATION_NAME; ?></title>
+	<meta name="title" content="<?php echo APPLICATION_NAME; ?>" />
 	<?php endif; ?>
 	
 	<?php if(!empty($page_description)): ?>
-		<meta name="description" content="<?php echo $page_description; ?>" />
+	<meta name="description" content="<?php echo $page_description; ?>" />
 	<?php endif; ?>
 	
 	<?php if(!empty($page_keywords)): ?>
-		<meta name="keywords" content="<?php echo $page_keywords; ?>" />
+	<meta name="keywords" content="<?php echo $page_keywords; ?>" />
 	<?php endif; ?>
 	
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
