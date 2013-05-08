c7129d5a425680ac7d1b78d83734bfb743e38fe7
Alan James <alanjames1987@gmail.com>
2013-05-08 15:24:18 -0400

    Commit

--- frontend/framework/js/core/MScript.js
+++ frontend/framework/js/core/MScript.js
@@ -767,8 +767,10 @@ var MSystemMessageContainer = (function() {
 		}
 
 		$('#MSystemMessageContainer').append(MSystemMessage);
-		$('.MSystemMessage').last().delay(fadeDelay).fadeOut(fadeSpeed, function() {
-			$(this).remove()
+		$('.MSystemMessage').last().delay(fadeDelay).fadeTo(fadeSpeed, 0, function() {
+			$(this).slideUp(250, function() {
+				$(this).remove();
+			});
 		});
 
 	};
