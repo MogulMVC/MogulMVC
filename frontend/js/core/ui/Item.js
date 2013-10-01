bc614278dfaf625f522de8900bbaf1ddc9cfa235
Alan James <ajames@Landmine.(none)>
2013-10-01 04:26:40 -0400

    Adds some new folders and JS files for better organization.

--- frontend/js/core/ui/Item.js
+++ frontend/js/core/ui/Item.js
@@ -2,10 +2,15 @@ var Item = (function() {
 
 	function Item(object) {
 
+		// validate variables
 		if (object.property == undefined) {
 			object.property = '';
 		}
+		
+		// adjust variables
+		object.property = object.property.toString();
 
+		// create ui element
 		var ui = document.createElement('DOM_ELEMENT_HERE');
 
 		ui.id = 'SOMETHING_' + id;
